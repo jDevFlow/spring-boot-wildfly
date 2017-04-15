ead79f4eedaddb2c9d16e15e41fc555c8878d465
robsonapsilva <robsonapsilva@hotmail.com>
2017-04-15 00:57:04 -0300

    Initial Structure

---
+++ src/main/java/com/robsonapsilva/SpringBootWildflyApplication.java
@@ -0,0 +1,24 @@
+package com.robsonapsilva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * 
+ * @author robsonapsilva
+ *
+ */
+@SpringBootApplication
+public class SpringBootWildflyApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWildflyApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(getClass());
+	}
+}
